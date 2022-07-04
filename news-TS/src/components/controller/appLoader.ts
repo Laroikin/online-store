8cbab3f30bf670969a04db37bba5481521eb5f4d
Laroikin <said24abdi04@gmail.com>
2022-07-04 23:55:42 +0900

    feat: eslint config

--- news-TS/src/components/controller/appLoader.ts
+++ news-TS/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '05f9e2bb90524bffbd50d91342d6759d', // получите свой ключ https://newsapi.org/
         });
     }
 }
