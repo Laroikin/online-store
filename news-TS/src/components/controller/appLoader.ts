070365f8b3fda7dc15cacc3211779fa2395b7f97
Laroikin <said24abdi04@gmail.com>
2022-07-07 12:54:22 +0900

    refactor: change api key

--- news-TS/src/components/controller/appLoader.ts
+++ news-TS/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '05f9e2bb90524bffbd50d91342d6759d', // получите свой ключ https://newsapi.org/
+            apiKey: '0a1151c74c14421a943cbd5c16858096', // получите свой ключ https://newsapi.org/
         });
     }
 }
